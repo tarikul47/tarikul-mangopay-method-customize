461218d69122cc77581f570ba6ee1e4b8211c59d
Tarikul Islam <tarikul47@gmail.com>
2024-03-18 16:51:09 +0600

    first commit

---
+++ formdata.php
@@ -0,0 +1,351 @@
+<?php
+Array
+(
+    [store_name] => wcfmlancer
+    [social] => Array
+        (
+        )
+
+    [payment] => Array
+        (
+            [method] => mangopay
+            [paypal] => Array
+                (
+                    [email] => 
+                )
+
+            [skrill] => Array
+                (
+                    [email] => 
+                )
+
+            [mangopay] => Array
+                (
+                    [birthday] => November 18, 1989
+                    [nationality] => ES
+                    [billing_country] => ES
+                    [legal_email] => jon@yahoo.com
+                    [compagny_number] => B12345678
+                    [hq_address] => 34 rue des Entreprises
+                    [hq_address2] => 
+                    [hq_city] => Paris
+                    [hq_region] => Ile-de-France
+                    [hq_postalcode] => 75051
+                    [hq_country] => ES
+                    [kyc_details] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [type] => IDENTITY_PROOF
+                                    [file] => 104
+                                )
+
+                        )
+
+                    [bank_details] => Array
+                        (
+                            [vendor_account_type] => IBAN
+                            [vendor_account_name] => Joe Blogs
+                            [vendor_account_address1] => 1 Mangopay Street
+                            [vendor_account_address2] => 
+                            [vendor_account_city] => Paris
+                            [vendor_account_region] => Ile de France
+                            [vendor_account_postcode] => 75001
+                            [vendor_account_country] => ES
+                            [vendor_iban] => FR76*******************0143
+                            [vendor_bic] => ******PP
+                            [vendor_gb_accountnumber] => 
+                            [sort_code] => 
+                            [vendor_us_accountnumber] => 
+                            [vendor_us_aba] => 
+                            [vendor_us_datype] => CHECKING
+                            [vendor_ca_bankname] => 
+                            [vendor_ca_instnumber] => 
+                            [vendor_ca_branchcode] => 
+                            [vendor_ca_accountnumber] => 
+                            [vendor_ot_country] => 
+                            [vendor_ot_bic] => 
+                            [vendor_ot_accountnumber] => 
+                        )
+
+                )
+
+            [bank] => Array
+                (
+                    [ac_name] => 
+                    [ac_number] => 
+                    [bank_name] => 
+                    [bank_addr] => 
+                    [routing_number] => 
+                    [iban] => 
+                    [swift] => 
+                    [ifsc] => 
+                )
+
+        )
+
+    [phone] => 
+    [show_email] => no
+    [location] => 
+    [find_address] => 
+    [banner] => 
+    [customer_support] => Array
+        (
+            [phone] => 
+            [email] => 
+            [address1] => 
+            [address2] => 
+            [country] => 
+            [city] => 
+            [state] => 
+            [zip] => 
+        )
+
+    [store_slug] => wcfmlancer
+    [store_email] => wcfmlancer@gmail.com
+    [gravatar] => 
+    [banner_type] => single_img
+    [banner_video] => 
+    [banner_slider] => Array
+        (
+            [0] => Array
+                (
+                    [image] => 
+                    [link] => 
+                )
+
+        )
+
+    [mobile_banner] => 
+    [list_banner_type] => single_img
+    [list_banner] => 
+    [list_banner_video] => 
+    [shop_description] => <p><br></p>
+    [store_name_position] => on_banner
+    [store_ppp] => 10
+    [address] => Array
+        (
+            [street_1] => 
+            [street_2] => 
+            [city] => 
+            [zip] => 
+            [country] => 
+            [state] => 
+        )
+
+    [geolocation] => Array
+        (
+            [store_location] => 
+            [store_lat] => 30.0599153
+            [store_lng] => 31.2620199
+        )
+
+    [mangopay_vendor_id] => 6
+    [wcfmmp_shipping] => Array
+        (
+            [_wcfmmp_user_shipping_enable] => yes
+            [_wcfmmp_pt] => 
+            [_wcfmmp_user_shipping_type] => by_zone
+        )
+
+    [wcfmmp_shipping_by_country] => Array
+        (
+            [_wcfmmp_shipping_type_price] => 
+            [_wcfmmp_additional_product] => 
+            [_wcfmmp_additional_qty] => 
+            [_free_shipping_amount] => 
+            [_local_pickup_cost] => 
+            [_wcfmmp_form_location] => 
+        )
+
+    [wcfmmp_shipping_rates] => Array
+        (
+            [0] => Array
+                (
+                    [wcfmmp_country_to] => 
+                    [wcfmmp_country_to_price] => 0
+                    [wcfmmp_shipping_state_rates] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [wcfmmp_state_to] => 
+                                    [wcfmmp_state_to_price] => 0
+                                )
+
+                        )
+
+                )
+
+        )
+
+    [wcfmmp_shipping_by_weight] => Array
+        (
+            [_free_shipping_amount] => 
+            [_local_pickup_cost] => 
+        )
+
+    [wcfmmp_shipping_rates_by_weight] => Array
+        (
+            [0] => Array
+                (
+                    [wcfmmp_weightwise_country_to] => 
+                    [wcfmmp_weightwise_country_mode] => by_rule
+                    [wcfmmp_weightwise_country_per_unit_cost] => 0
+                    [wcfmmp_weightwise_country_default_cost] => 0
+                    [wcfmmp_shipping_country_weight_settings] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [wcfmmp_weight_rule] => up_to
+                                    [wcfmmp_weight_unit] => 
+                                    [wcfmmp_weight_price] => 
+                                )
+
+                        )
+
+                )
+
+        )
+
+    [store_seo] => Array
+        (
+            [wcfmmp-seo-meta-title] => 
+            [wcfmmp-seo-meta-desc] => 
+            [wcfmmp-seo-meta-keywords] => 
+            [wcfmmp-seo-og-title] => 
+            [wcfmmp-seo-og-desc] => 
+            [wcfmmp-seo-og-image] => 
+            [wcfmmp-seo-twitter-title] => 
+            [wcfmmp-seo-twitter-desc] => 
+            [wcfmmp-seo-twitter-image] => 
+        )
+
+    [wcfm_policy_tab_title] => 
+    [wcfm_shipping_policy] => <p><br></p>
+    [wcfm_refund_policy] => <p><br></p>
+    [wcfm_cancellation_policy] => <p><br></p>
+    [wcfm_store_hours] => Array
+        (
+            [day_times] => Array
+                (
+                    [0] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [start] => 
+                                    [end] => 
+                                )
+
+                        )
+
+                    [1] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [start] => 
+                                    [end] => 
+                                )
+
+                        )
+
+                    [2] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [start] => 
+                                    [end] => 
+                                )
+
+                        )
+
+                    [3] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [start] => 
+                                    [end] => 
+                                )
+
+                        )
+
+                    [4] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [start] => 
+                                    [end] => 
+                                )
+
+                        )
+
+                    [5] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [start] => 
+                                    [end] => 
+                                )
+
+                        )
+
+                    [6] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [start] => 
+                                    [end] => 
+                                )
+
+                        )
+
+                )
+
+        )
+
+    [wcfm_nonce] => 9bbdf0a703
+    [store_hide_email] => no
+    [store_hide_phone] => no
+    [store_hide_address] => no
+    [store_hide_map] => no
+    [store_hide_description] => no
+    [store_hide_policy] => no
+    [store_location] => 
+    [store_lat] => 30.0599153
+    [store_lng] => 31.2620199
+    [ubo_mp_id] => 209258010
+    [mangopay_termsAndConditionsAccepted] => yes
+    [FirstName] => 
+    [LastName] => 
+    [AddressLine1] => 
+    [AddressLine2] => 
+    [City] => 
+    [Region] => 
+    [PostalCode] => 
+    [ubo_Country_select] => 
+    [ubo_NationalityCountry_select] => 
+    [Birthday] => 
+    [BirthplaceCity] => 
+    [ubo_BirthplaceCountry_select] => 
+    [mangopay_upload_kyc_status] => yes
+    [mangopay_add_bank] => yes
+    [mangopay_add_bank_status] => no
+    [mangopay_upload_kyc] => yes
+)
+
+[24-Nov-2023 09:25:35 UTC] message stop 1
+[24-Nov-2023 09:25:35 UTC] message stop 2
+[24-Nov-2023 09:25:35 UTC] kyc is array
+[24-Nov-2023 09:25:35 UTC] MangoPay\KycDocument Object
+(
+    [Id] => 
+    [Tag] => wp_user_id:6
+    [CreationDate] => 
+    [RefusedReasonType] => 
+    [RefusedReasonMessage] => 
+    [ProcessedDate] => 
+    [Type] => IDENTITY_PROOF
+    [Status] => 
+    [UserId] => 
+    [Flags] => 
+)
+
